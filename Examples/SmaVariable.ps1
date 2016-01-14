67db4d83ef95fc1e01b52b67dee3dc6fd909d804
dcrreynolds <dcr.reynolds@outlook.com>
2016-01-14 13:23:02 -0800

    Added example, fixed failing test

---
+++ Examples/SmaVariable.ps1
@@ -0,0 +1,26 @@
+﻿Configuration RunbookDirectory 
+{ 
+    Param 
+    ( 
+        $NodeName 
+    ) 
+
+
+    Import-DscResource -ModuleName 'xSCSMA' 
+
+    Node $NodeName 
+    { 
+        SmaVariable Example 
+        { 
+            Name = "Variable"
+            Value = 'Value' 
+            WebServiceEndpoint = 'https:\\localhost' 
+        } 
+    } 
+} 
+
+
+RunbookDirectory -NodeName "localhost" -OutputPath .\SMA 
+
+
+Start-DscConfiguration -Path .\SMA -Wait -Force -Verbose 
